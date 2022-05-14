4ad66a7e7a3cd3be15aaffaba2aa924b4f19da37
Flavio Lecca <fla.lecca31@gmail.com>
2022-05-14 17:48:16 -0500

    Inicio de Proyecto

---
+++ Prueba/Prueba/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prueba
+//
+//  Created by Flavio Lecca Chavez on 14/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
